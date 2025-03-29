3edb9badf7d279e0df9929373fa40087cce6367a
dennisguim <drinaldim@gmail.com>
2025-03-29 08:54:34 -0300

    Update 9.py

--- exercises/9.py
+++ exercises/9.py
@@ -1,5 +1,24 @@
 import random
+import os
 
-n = int(input('Adivinhe um número de 1 a 9: '))
 i = random.randint(1, 9)
 
+
+while True:
+    n = input('Adivinhe um número de 1 a 9: ')
+    os.system('clear')
+    
+    if n.isdigit():
+        if int(n) == i:
+            print('Você acertou! 👏')
+            print()
+            i = random.randint(1, 9)
+        else:
+            print('Errou! 🚫 Tente novamente.')
+            print()
+    elif n.lower() == 'sair':
+        break
+    else:
+        print('Ooops, algo deu errado. 🤷')
+        print()
+        
