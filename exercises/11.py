da085ed058ec3fa600d234c354afb0c405a7a11d
dennisguim <drinaldim@gmail.com>
2025-03-30 10:29:32 -0300

    Create 11.py

---
+++ exercises/11.py
@@ -0,0 +1,27 @@
+# Check Primality Functions 
+
+def primo(x):
+    nums = [2, 3, 5, 7]
+    check = []
+
+    for i in nums:
+        p = x % i
+        check.append(p)
+    
+    if x == 2 or x == 3 or x == 5 or x == 7:
+        return f'{x} é um número primo.'
+    else:
+        if 0 in check:
+            return f'{x} não é um número primo.'
+        else:
+            return f'{x} é um número primo'
+
+x = input("Digite um número para checar se ele é primo ou não: ")
+x_1 = ""
+for y in x:
+   if y.isdigit():
+       x_1 += y
+    
+       
+
+print(primo(int(x_1)))
