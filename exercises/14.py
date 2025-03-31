3783c6fcdc8732d14af904a1268bbf98db2315d1
dennisguim <drinaldim@gmail.com>
2025-03-31 20:58:02 -0300

    Create 14.py

---
+++ exercises/14.py
@@ -0,0 +1,24 @@
+# List Remove Duplicates | returns a new list that contains all the elements of the first list minus all the duplicates.
+
+lista_a = [1, 2, 3, 4, 5, 6, 6, 2, 2, 1, 3, 4]
+lista_b = []
+lista_c = []
+
+# gerar uma lista com loop
+def com_loop(): 
+    for i in lista_a:
+        if i not in lista_b:
+            lista_b.append(i)
+        else:
+            continue
+    return lista_b
+            
+                  
+# gerar uma lista com set()
+def com_set():
+    lista_c = list(set(lista_a))
+    return lista_c
+
+print(com_loop())
+
+print(com_set())
