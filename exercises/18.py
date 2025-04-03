0eb75c1d54feb4d6b731680723e1e6dcfde26037
dennisguim <drinaldim@gmail.com>
2025-04-03 11:15:05 -0300

    Create 18.py

---
+++ exercises/18.py
@@ -0,0 +1,12 @@
+# Cows And Bulls
+
+import random
+
+numero_random = random.randint(1000, 9999)
+
+numero_jogador = 1345
+
+for numero in numero_jogador:
+    if numero in numero_random:
+        ...
+    if numero[n]
